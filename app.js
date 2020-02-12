601d76c22d1da67a0c7aad4f3ea09a9d7fc9e8be
Branislav Trstenský <bt7s7k7@hotmail.sk>
2020-02-12 13:18:18 +0100

    Basic drawable rendering

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
 /** @type {HTMLCanvasElement} */
 var canvas = document.getElementById("canvas")
-var ctx = canvas.getContext("2d", { alpha: true })
+
+/** @typedef {{x : number, y : number, width : number, height : number}} Rect */
+
+var newRect = () => ({ x: 0, y: 0, width: 0, height: 0 })
+
+class Drawable {
+    constructor(image = null) {
+        /** @type */
+        this.image = null
+        this.rect = newRect()
+        this.sourceRect = newRect()
+    }
+
+    /**
+     * @param {number} oX
+     * @param {number} oY
+     * @param {CanvasRenderingContext2D} ctx
+     */
+    draw(oX, oY, ctx) {
+        ctx.fillStyle = "white"
+        ctx.fillRect(this.rect.x + oX, this.rect.y + oY, this.rect.width, this.rect.height)
+    }
+}
+class Renderer {
+    /**
+     * @param {HTMLCanvasElement} canvas 
+     */
+    constructor(canvas) {
+        /** @type {Drawable[]} */
+        this.drawables = []
+        this.oX = 0
+        this.oY = 0
+        this.canvas = canvas
+        this.ctx = canvas.getContext("2d", { alpha: true })
+    }
+
+    draw() {
+        var clientRect = canvas.getBoundingClientRect()
+        canvas.width = clientRect.width
+        canvas.height = clientRect.height
+
+        for (let drawable of this.drawables) {
+            drawable.draw(Math.floor(this.oX + clientRect.width / 2), Math.floor(this.oY + clientRect.height / 2), this.ctx)
+        }
+    }
+}
+
+var renderer = new Renderer(canvas)
+
+function update() {
+    renderer.draw()
+
+    requestAnimationFrame(update)
+}
+
+update()
